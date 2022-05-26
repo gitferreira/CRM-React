b8e1e53f7f1516a7682c8e65ae241cd0b3533684
gitferreira <iniferre96@gmail.com>
2022-05-26 17:42:58 +0200

    Commit

--- src/components/CustomerForm.jsx
+++ src/components/CustomerForm.jsx
@@ -6,6 +6,7 @@ const CustomerForm = () => {
     <div className='bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto'>
         <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>Add Customer</h1>
         <Formik>
+          {/* Formik form */}
             <Form className='mt-10'>
               <div className='mb-4'>
                 <label className='text-gray-800' htmlFor='name'>Name</label>
